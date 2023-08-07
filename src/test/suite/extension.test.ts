0ede00e701ca6f0ebef4aa92e0d6aff218a83c20
Rene Saarsoo <nene@triin.net>
2023-08-07 15:47:47 +0300

    Smoke-test for formatting SQL file

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,26 @@
+import * as path from 'path';
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.');
-
-  test('Sample test', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+suite('SQL Formatter VSCode', () => {
+  test('formats SQL file', async () => {
+    const uri = vscode.Uri.file(path.join(__dirname + '/../../../test-data/example.sql'));
+    const document = await vscode.workspace.openTextDocument(uri);
+    await vscode.window.showTextDocument(document);
+    assert.equal(
+      'SELECT * FROM my_table WHERE age > 10 AND salary BETWEEN 1000 AND 2000;\n',
+      document.getText(),
+    );
+    await vscode.commands.executeCommand('editor.action.formatDocument');
+    assert.equal(
+      `SELECT
+    *
+FROM
+    my_table
+WHERE
+    age > 10
+    AND salary BETWEEN 1000 AND 2000;`,
+      document.getText(),
+    );
   });
 });
