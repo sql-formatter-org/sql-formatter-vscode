14241cccbf95909a58eb205874dc67d954553fd9
Rene Saarsoo <nene@triin.net>
2023-08-07 20:01:29 +0300

    Extract registerFormattingProviderForEachDialect()

--- src/extension.ts
+++ src/extension.ts
@@ -4,18 +4,19 @@ import { sqlDialects } from './sqlDialects';
 import { formatSelection } from './formatSelection';
 
 export function activate(context: vscode.ExtensionContext) {
-  // add SQL-Formatter-VSCode as a format provider for each SQL dialect
-  Object.entries(sqlDialects).forEach(([vscodeLang, prettierLang]) =>
-    context.subscriptions.push(
-      vscode.languages.registerDocumentFormattingEditProvider(
-        vscodeLang,
-        new SqlFormattingProvider(prettierLang),
-      ),
-    ),
-  );
-
   context.subscriptions.push(
     vscode.commands.registerCommand('sql-formatter-vscode.format-selection', formatSelection),
+
+    ...registerFormattingProviderForEachDialect(),
+  );
+}
+
+function registerFormattingProviderForEachDialect() {
+  return Object.entries(sqlDialects).map(([vscodeLang, sqlDialectName]) =>
+    vscode.languages.registerDocumentFormattingEditProvider(
+      vscodeLang,
+      new SqlFormattingProvider(sqlDialectName),
+    ),
   );
 }
 
