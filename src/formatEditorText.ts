553c7ed3f3498e002bd9be048552fa0e8d4ad703
Rene Saarsoo <nene@triin.net>
2023-08-12 22:01:38 +0300

    Define endsWithNewline() inside formatEditorText.ts

--- src/formatEditorText.ts
+++ src/formatEditorText.ts
@@ -1,6 +1,7 @@
 import { FormatOptionsWithLanguage, format } from 'sql-formatter';
-import { endsWithNewline } from './stringUtils';
 
 export function formatEditorText(text: string, config: FormatOptionsWithLanguage): string {
   return format(text, config) + (endsWithNewline(text) ? '\n' : '');
 }
+
+const endsWithNewline = (text: string) => /\n$/.test(text);
--- src/stringUtils.ts
+++
@@ -1,1 +0,0 @@
-export const endsWithNewline = (text: string) => /\n$/.test(text);
